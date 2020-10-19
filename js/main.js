5f541fd55b757134f1c66d24be1f114ea12f7ce1
Ryan Xin <xinpf0715@gmail.com>
2020-10-19 23:13:15 +1100

    social media share toggle done'

--- js/main.js
+++ js/main.js
@@ -1,26 +1,23 @@
 window.onload = () => {
   const shareButton = document.getElementsByClassName("share-button")[0];
   const shareIcons = document.getElementsByClassName("share-icons-container")[0];
+  const shareIcon = document.getElementsByClassName("share-icon")[0];
   
-  shareButton.addEventListener("click", () => {
-    // if (shareButton.style.backgroundColor === "hsl(214, 17%, 51%)") {
-    //   shareButton.style.backgroundColor = "hsl(210, 46%, 95%)"
-    // } else {
-    //   shareButton.style.backgroundColor = "hsl(214, 17%, 51%)"
-    // }
-    
+  shareButton.addEventListener("mouseenter", (e) => {
+    e.target.style.backgroundColor = "hsl(214, 17%, 51%)";
+    shareIcon.style.fill = "hsl(210, 46%, 95%)";
+  });
+  
+  shareButton.addEventListener("mouseleave", (e) => {
+    e.target.style.backgroundColor = "hsl(210, 46%, 95%)";
+    shareIcon.style.fill = "hsl(214, 17%, 51%)";
+  });
+  
+  shareButton.addEventListener("click", () => {    
     if (shareIcons.classList.contains("hide")) {
       shareIcons.classList.remove("hide");
     } else {
       shareIcons.classList.add("hide");
     }
-  });
-  
-  shareButton.addEventListener("mouseenter", (e) => {
-    e.target.style.backgroundColor = "hsl(214, 17%, 51%)";
-  });
-  
-  shareButton.addEventListener("mouseleave", (e) => {
-    e.target.style.backgroundColor = "hsl(210, 46%, 95%)";
   });
 }
